18fd89544c7f94e1eea56617a54d19ed542322d0
Giulio Giovanella <giuliog@furb.br>
2020-03-11 16:24:39 -0300

    finished

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser: true, useUnifiedTopology: true });
 
 requireDir('./src/models');
 
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
+routes.put("/products/:id", ProductController.update);
+routes.delete("/products/:id", ProductController.destroy);
 routes.post("/products", ProductController.store);
+routes.get("/products/:id", ProductController.show);
 routes.get("/products", ProductController.index);
-
 
 module.exports = routes;
